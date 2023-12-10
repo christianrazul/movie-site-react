52ba71c74f13cc068a94308a74b494771dcb9b07
christianrazul <razulchristian@gmail.com>
2023-12-11 03:25:19 +0800

    made search functionality work

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import "./App.css";
 import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
 
-// fe2a75ae
-
 const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=fe2a75ae";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -25,8 +24,8 @@ const App = () => {
     <div className="app">
       <h1>MovieLand</h1>
       <div className="search">
-        <input placeholder="Search for movies" value={"Superman"} onChange={() => {}} />
-        <img src={SearchIcon} alt="search" onClick={() => {}} />
+        <input placeholder="Search for movies" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+        <img src={SearchIcon} alt="search" onClick={() => searchMovies(searchTerm)} />
       </div>
       {movies?.length > 0 ? (
         <div className="container">
